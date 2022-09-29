ed7c2c37fb716cd3f6c1733801d0bab9597ccc15
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-09-29 20:32:33 +0530

    Item page new Item add Model code

--- assignment/CSS/Frameworks/POS/view/assets/styles/script/item.js
+++ assignment/CSS/Frameworks/POS/view/assets/styles/script/item.js
@@ -2,3 +2,45 @@
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
+
+/**
+ * @ Global Scope
+ */
+var items = [];
+
+/**
+ * Button Add New Item
+ * */
+$("#btnISave").click(function () {
+
+    //select all the four text fields and then get their typed values
+    let itemCode = $("#txtItemsId").val();
+    let itemName = $("#txtItemName").val();
+    let itemQty = $("#txtItemQty").val();
+    let itemUnitPrice = $("#txtItemPrice").val();
+
+
+    //Alert Save
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Item has been saved',
+        showConfirmButton: false,
+        timer: 2500
+    })
+
+    // items object
+    var itemObject = {
+        code: itemCode,
+        name: itemName,
+        qty: itemQty,
+        price: itemUnitPrice
+    }
+
+    //Add the item object to the array
+    items.push(itemObject);
+
+     console.log(items);
+
+
+});
