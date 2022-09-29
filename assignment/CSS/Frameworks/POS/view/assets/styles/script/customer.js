4d216d3783f28e8919ba3865bec813b8e09753ba
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-09-29 19:41:40 +0530

    Customer page Add new Customer Save Code

---
+++ assignment/CSS/Frameworks/POS/view/assets/styles/script/customer.js
@@ -0,0 +1,45 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+/**
+ * @ Global Scope
+ */
+var customers = [];
+
+/**
+ * Button Add New Customer
+ * */
+$("#btnCSave").click(function () {
+
+    //select all the four text fields and then get their typed values
+    let customerID = $("#txtCustomerId").val();
+    let customerName = $("#txtCustomerName").val();
+    let customerAddress = $("#txtCustomerAddress").val();
+    let customerSalary = $("#txtCustomerSalary").val();
+
+
+    //Alert Save
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Customer has been saved',
+        showConfirmButton: false,
+        timer: 2500
+    })
+
+    // customer object
+    var customerObject = {
+        id: customerID,
+        name: customerName,
+        address: customerAddress,
+        salary: customerSalary
+    }
+
+    //Add the customer object to the array
+    customers.push(customerObject);
+
+    console.log(customers);
+
+});
