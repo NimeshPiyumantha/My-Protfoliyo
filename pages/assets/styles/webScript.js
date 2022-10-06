125dcbf93b50c9b9147f24c97a4095f87933bc4a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-10-06 12:40:39 +0530

    Add Button Setion in web

---
+++ pages/assets/styles/webScript.js
@@ -0,0 +1,84 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+$("#all").click(function () {
+    $("#1").css('display', 'block');
+    $("#2").css('display', 'block');
+    $("#3").css('display', 'block');
+    $("#4").css('display', 'block');
+    $("#5").css('display', 'block');
+    $("#6").css('display', 'block');
+    $("#7").css('display', 'block');
+    $("#8").css('display', 'block');
+    $("#9").css('display', 'block');
+    $("#10").css('display', 'block');
+    $("#11").css('display', 'block');
+    $("#12").css('display', 'block');
+    $("#13").css('display', 'block');
+});
+
+$("#basics").click(function () {
+    $("#1").css('display', 'none');
+    $("#2").css('display', 'none');
+    $("#3").css('display', 'none');
+    $("#4").css('display', 'none');
+    $("#5").css('display', 'none');
+    $("#6").css('display', 'none');
+    $("#7").css('display', 'none');
+    $("#8").css('display', 'block');
+    $("#9").css('display', 'block');
+    $("#10").css('display', 'block');
+    $("#11").css('display', 'block');
+    $("#12").css('display', 'block');
+    $("#13").css('display', 'block');
+});
+
+$("#transitions").click(function () {
+    $("#1").css('display', 'none');
+    $("#2").css('display', 'none');
+    $("#3").css('display', 'none');
+    $("#4").css('display', 'none');
+    $("#5").css('display', 'none');
+    $("#6").css('display', 'block');
+    $("#7").css('display', 'block');
+    $("#8").css('display', 'none');
+    $("#9").css('display', 'none');
+    $("#10").css('display', 'none');
+    $("#11").css('display', 'none');
+    $("#12").css('display', 'none');
+    $("#13").css('display', 'none');
+});
+
+$("#animations").click(function () {
+    $("#1").css('display', 'block');
+    $("#2").css('display', 'block');
+    $("#3").css('display', 'block');
+    $("#4").css('display', 'block');
+    $("#5").css('display', 'none');
+    $("#6").css('display', 'none');
+    $("#7").css('display', 'none');
+    $("#8").css('display', 'none');
+    $("#9").css('display', 'none');
+    $("#10").css('display', 'none');
+    $("#11").css('display', 'none');
+    $("#12").css('display', 'none');
+    $("#13").css('display', 'none');
+});
+
+$("#frameworks").click(function () {
+    $("#1").css('display', 'none');
+    $("#2").css('display', 'none');
+    $("#3").css('display', 'none');
+    $("#4").css('display', 'none');
+    $("#5").css('display', 'block');
+    $("#6").css('display', 'none');
+    $("#7").css('display', 'none');
+    $("#8").css('display', 'none');
+    $("#9").css('display', 'none');
+    $("#10").css('display', 'none');
+    $("#11").css('display', 'none');
+    $("#12").css('display', 'none');
+    $("#13").css('display', 'none');
+});
