e5f45c2e562a748fedbe586ba9e5b43d79a4efa2
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-10-06 20:13:43 +0530

    Add Loader

---
+++ assets/styles/script.js
@@ -0,0 +1,12 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+    // execute after all the content fully loaded
+    $(window).on('load', function () {
+        console.log("Window on load");
+
+        // $("#loader").css('display','none');
+        $("#loader").fadeOut(1000);
+});
