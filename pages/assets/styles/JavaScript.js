ba5762b62a491ecd14acb3a6487b25753cb07a0b
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-10-06 13:21:49 +0530

    Add Button Setion in java

---
+++ pages/assets/styles/JavaScript.js
@@ -0,0 +1,37 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+$("#all").click(function () {
+    $("#1").css('display', 'block');
+    $("#2").css('display', 'block');
+    $("#3").css('display', 'block');
+    $("#4").css('display', 'block');
+    $("#5").css('display', 'block');
+    $("#6").css('display', 'block');
+    $("#7").css('display', 'block');
+    $("#8").css('display', 'block');
+});
+
+$("#java").click(function () {
+    $("#1").css('display', 'block');
+    $("#2").css('display', 'block');
+    $("#3").css('display', 'block');
+    $("#4").css('display', 'block');
+    $("#5").css('display', 'block');
+    $("#6").css('display', 'block');
+    $("#7").css('display', 'none');
+    $("#8").css('display', 'none');
+});
+
+$("#javaScript").click(function () {
+    $("#1").css('display', 'none');
+    $("#2").css('display', 'none');
+    $("#3").css('display', 'none');
+    $("#4").css('display', 'none');
+    $("#5").css('display', 'none');
+    $("#6").css('display', 'none');
+    $("#7").css('display', 'block');
+    $("#8").css('display', 'block');
+});
