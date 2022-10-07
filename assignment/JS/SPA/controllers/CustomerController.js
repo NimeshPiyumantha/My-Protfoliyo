45b9ad0209753854b2165bce105367e9fe74f609
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-10-07 17:18:35 +0530

    Refactor Costomer Model

--- assignment/JS/SPA/controllers/CustomerController.js
+++ assignment/JS/SPA/controllers/CustomerController.js
@@ -4,116 +4,8 @@
  **/
 
 /**
- * load all customers Button
- * */
-$("#btnViewAllCustomer").click(function () {
-    loadAllCustomers();
-});
-
-/**
- * Table Listener Click and Load textFields
- * */
-function blindClickEvents() {
-    $("#customerTable>tr").click(function () {
-        let id = $(this).children().eq(0).text();
-        let name = $(this).children().eq(1).text();
-        let address = $(this).children().eq(2).text();
-        let salary = $(this).children().eq(3).text();
-        console.log(id, name, address, salary);
-
-        $("#searchCustomerId").val(id);
-        $("#nameUpdate").val(name);
-        $("#addressUpdate").val(address);
-        $("#salaryUpdate").val(salary);
-
-        $("#searchCIdDelete").val(id);
-        $("#disabledNameDelete").val(name);
-        $("#disabledAddressDelete").val(address);
-        $("#disabledSalaryDelete").val(salary);
-    });
-}
-
-/**
- * Table Listener double click and Click and Remove textFields
- * */
-$("#customerTable").dblclick(function () {
-    Swal.fire({
-        title: 'Do you want to Delete the Select row?',
-        showDenyButton: true,
-        showCancelButton: true,
-        confirmButtonText: 'Yes',
-        denyButtonText: 'No',
-        customClass: {
-            actions: 'my-actions',
-            cancelButton: 'order-1 right-gap',
-            confirmButton: 'order-2',
-            denyButton: 'order-3',
-        }
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $(this).children('tr').eq(0).remove();
-            Swal.fire('Delete!', '', 'success')
-        } else if (result.isDenied) {
-            Swal.fire('Select row are not Delete', '', 'info')
-        }
-    })
-
-});
-
-
-/**
- * Search id and Load Table
- * */
-$("#btnSearchCus").click(function () {
-    var result = customers.find(({id}) => id === $("#searchCusId").val());
-    console.log(result);
-
-    $("#customerTable").empty();
-    var row = `<tr><td>${result.id}</td><td>${result.name}</td><td>${result.address}</td><td>${result.salary}</td></tr>`;
-    $("#customerTable").append(row);
-
-    $("#searchCustomerId").val(result.id);
-    $("#nameUpdate").val(result.name);
-    $("#addressUpdate").val(result.address);
-    $("#salaryUpdate").val(result.salary);
-
-    $("#searchCIdDelete").val(result.id);
-    $("#disabledNameDelete").val(result.name);
-    $("#disabledAddressDelete").val(result.address);
-    $("#disabledSalaryDelete").val(result.salary);
-
-});
-
-/**
- * Auto Forces Input Fields Search
- * */
-$(document).ready(function () {
-    $('#searchCusId').keypress(function (event) {
-        if (event.which === 13) {
-            $('#btnSearchCus').focus();
-        }
-    });
-    $('#btnSearchCus').keypress(function (event) {
-        if (event.which === 13) {
-            $('#searchCusId').focus();
-        }
-    });
-});
-
-/**
- * clear Search input fields Values Button
- * */
-$("#clearSearchCus").click(function () {
-    searchCusId.value = '';
-    clearDTextFields();
-    clearUTextFields();
-});
-
-
-/**
  * Save Model
  * */
-
 
 /**
  * Button Add New Customer
@@ -155,97 +47,129 @@ $("#btnCSave").click(function () {
 /**
  * Auto Forces Input Fields Save
  * */
-var regExCusID = /^(C00-)[0-9]{3,4}$/;
-var regExCusName = /^[A-z ]{3,20}$/;
-var regExCusAddress = /^[A-z0-9/ ]{4,30}$/;
-var regExSalary = /[0-9]{4,19}$/;
+$("#txtCustomerId").focus();
+const regExCusID = /^(C00-)[0-9]{3,4}$/;
+const regExCusName = /^[A-z ]{3,20}$/;
+const regExCusAddress = /^[A-z0-9/ ]{4,30}$/;
+const regExSalary = /[0-9]{4,19}$/;
 
+let customerValidations = [];
+customerValidations.push({reg: regExCusID, field: $('#txtCustomerId'),error:'Customer ID Pattern is Wrong : C00-001'});
+customerValidations.push({reg: regExCusName, field: $('#txtCustomerName'),error:'Customer Name Pattern is Wrong : A-z 5-20'});
+customerValidations.push({reg: regExCusAddress, field: $('#txtCustomerAddress'),error:'Customer Address Pattern is Wrong : A-z 0-9 ,/'});
+customerValidations.push({reg: regExSalary, field: $('#txtCustomerSalary'),error:'Customer Salary Pattern is Wrong : 100 or 100.00'});
+
+//disable tab key of all four text fields using grouping selector in CSS
 $("#txtCustomerId,#txtCustomerName,#txtCustomerAddress,#txtCustomerSalary").on('keydown', function (event) {
     if (event.key == "Tab") {
         event.preventDefault();
     }
 });
 
-$('#txtCustomerId').keypress(function (event) {
-    let input = $("#txtCustomerId").val();
 
-    if (regExCusID.test(input)) {
-        $("#txtCustomerId").css('border', '2px solid green');
-        $("#lblCusID").text("");
+$("#txtCustomerId,#txtCustomerName,#txtCustomerAddress,#txtCustomerSalary").on('keyup', function (event) {
+    checkValidity();
+});
 
-        if (event.which === 13) {
-            $('#txtCustomerName').focus();
-        }
+$("#txtCustomerId,#txtCustomerName,#txtCustomerAddress,#txtCustomerSalary").on('blur', function (event) {
+    checkValidity();
+});
+
+
+$("#txtCustomerId").on('keydown', function (event) {
+    if (event.key == "Enter" && check(regExCusID, $("#txtCustomerId"))) {
+        $("#txtCustomerName").focus();
     } else {
-        $("#txtCustomerId").css('border', '2px solid red');
-        $("#lblCusID").text("Wrong format : C00-001");
+        focusText($("#txtCustomerId"));
     }
 });
 
-$('#txtCustomerName').keypress(function (event) {
-    let input = $("#txtCustomerName").val();
 
-    if (regExCusName.test(input)) {
-        $("#txtCustomerName").css('border', '2px solid green');
-        $("#lblCusName").text("");
+$("#txtCustomerName").on('keydown', function (event) {
+    if (event.key == "Enter" && check(regExCusName, $("#txtCustomerName"))) {
+        focusText($("#txtCustomerAddress"));
+    }
+});
 
-        if (event.which === 13) {
-            $('#txtCustomerAddress').focus();
+
+$("#txtCustomerAddress").on('keydown', function (event) {
+    if (event.key == "Enter" && check(regExCusAddress, $("#txtCustomerAddress"))) {
+        focusText($("#txtCustomerSalary"));
+    }
+});
+
+
+$("#txtCustomerSalary").on('keydown', function (event) {
+    if (event.key == "Enter" && check(regExSalary, $("#txtCustomerSalary"))) {
+        let res = confirm("Do you want to add this customer.?");
+        if (res) {
+            clearAllTexts();
         }
-    } else {
-        $("#txtCustomerName").css('border', '2px solid red');
-        $("#lblCusName").text("Wrong format : Nimesh");
     }
 });
 
-$('#txtCustomerAddress').keypress(function (event) {
-    let input = $("#txtCustomerAddress").val();
 
-    if (regExCusAddress.test(input)) {
-        $("#txtCustomerAddress").css('border', '2px solid green');
-        $("#lblCusAddress").text("");
-
-        if (event.which === 13) {
-            $('#txtCustomerSalary').focus();
+function checkValidity() {
+    let errorCount=0;
+    for (let validation of customerValidations) {
+        if (check(validation.reg,validation.field)) {
+            textSuccess(validation.field,"");
+        } else {
+            errorCount=errorCount+1;
+            setTextError(validation.field,validation.error);
         }
+    }
+    setButtonState(errorCount);
+}
+
+function check(regex, txtField) {
+    let inputValue = txtField.val();
+    return regex.test(inputValue) ? true : false;
+}
+
+function setTextError(txtField,error) {
+    if (txtField.val().length <= 0) {
+        defaultText(txtField,"");
     } else {
-        $("#txtCustomerAddress").css('border', '2px solid red');
-        $("#lblCusAddress").text("Wrong format : Galle");
+        txtField.css('border', '2px solid red');
+        txtField.parent().children('span').text(error);
     }
-});
+}
 
-$('#txtCustomerSalary').keypress(function (event) {
-    let input = $("#txtCustomerSalary").val();
-
-    if (regExSalary.test(input)) {
-        $("#txtCustomerSalary").css('border', '2px solid green');
-        $("#lblCusSalary").text("");
-
-        if (event.which === 13) {
-            $('#btnCSave').focus();
-        }
+function textSuccess(txtField,error) {
+    if (txtField.val().length <= 0) {
+        defaultText(txtField,"");
     } else {
-        $("#txtCustomerSalary").css('border', '2px solid red');
-        $("#lblCusSalary").text("Wrong format : 70000");
+        txtField.css('border', '2px solid green');
+        txtField.parent().children('span').text(error);
     }
-});
+}
 
-$('#btnCSave').keypress(function (event) {
-    if (event.which === 13) {
-        $('#txtCustomerId').focus();
+function defaultText(txtField,error) {
+    txtField.css("border", "1px solid #ced4da");
+    txtField.parent().children('span').text(error);
+}
+
+function focusText(txtField) {
+    txtField.focus();
+}
+
+function setButtonState(value){
+    if (value>0){
+        $("#btnCSave").attr('disabled',true);
+    }else{
+        $("#btnCSave").attr('disabled',false);
     }
-});
+}
 
 
 /**
  * clear input fields Values Method
  * */
 function clearTextFields() {
-    txtCustomerId.value = '';
-    txtCustomerName.value = '';
-    txtCustomerAddress.value = '';
-    txtCustomerSalary.value = '';
     $("#txtCustomerId").focus();
+    $("#txtCustomerId,#txtCustomerName,#txtCustomerAddress,#txtCustomerSalary").val("");
+    checkValidity();
 }
 
 /**
@@ -259,10 +183,8 @@ $("#btnClearC").click(function () {
  * load all customers Method
  * */
 function loadAllCustomers() {
-
     //remove all the table body content before adding data
     $("#customerTable").empty();
-
 
     // get all customer records from the array
     for (var customer of customers) {
@@ -277,165 +199,4 @@ function loadAllCustomers() {
     blindClickEvents();
 }
 
-/**
- * Update Model
- * */
 
-
-/**
- * Auto Forces Input Fields update
- * */
-
-$("#searchCustomerId,#nameUpdate,#addressUpdate,#salaryUpdate").on('keydown', function (event) {
-    if (event.key == "Tab") {
-        event.preventDefault();
-    }
-});
-
-$('#searchCustomerId').keypress(function (event) {
-    let input = $("#searchCustomerId").val();
-
-    if (regExCusID.test(input)) {
-        $("#searchCustomerId").css('border', '2px solid green');
-        $("#lblUCusId").text("");
-
-        if (event.which === 13) {
-            $('#nameUpdate').focus();
-        }
-    } else {
-        $("#searchCustomerId").css('border', '2px solid red');
-        $("#lblUCusId").text("Wrong format : C00-001");
-    }
-});
-
-$('#nameUpdate').keypress(function (event) {
-    let input = $("#nameUpdate").val();
-
-    if (regExCusName.test(input)) {
-        $("#nameUpdate").css('border', '2px solid green');
-        $("#lblUCusName").text("");
-
-        if (event.which === 13) {
-            $('#addressUpdate').focus();
-        }
-    } else {
-        $("#nameUpdate").css('border', '2px solid red');
-        $("#lblUCusName").text("Wrong format : Nimesh");
-    }
-});
-
-$('#addressUpdate').keypress(function (event) {
-    let input = $("#addressUpdate").val();
-
-    if (regExCusAddress.test(input)) {
-        $("#addressUpdate").css('border', '2px solid green');
-        $("#lblUCusAddress").text("");
-
-        if (event.which === 13) {
-            $('#salaryUpdate').focus();
-        }
-    } else {
-        $("#addressUpdate").css('border', '2px solid red');
-        $("#lblUCusAddress").text("Wrong format : Galle");
-    }
-});
-
-$('#salaryUpdate').keypress(function (event) {
-    let input = $("#salaryUpdate").val();
-
-    if (regExSalary.test(input)) {
-        $("#salaryUpdate").css('border', '2px solid green');
-        $("#lblUCusSalary").text("");
-
-        if (event.which === 13) {
-            $('#bntUpdateCustomer').focus();
-        }
-    } else {
-        $("#salaryUpdate").css('border', '2px solid red');
-        $("#lblUCusSalary").text("Wrong format : 70000");
-    }
-});
-
-$('#bntUpdateCustomer').keypress(function (event) {
-    if (event.which === 13) {
-        $('#searchCustomerId').focus();
-    }
-});
-
-
-/*
-/!**
- * Update Model
- * Search id Enter Pressed And Load TextFields
- * *!/
-$("#searchCustomerId").keyup(function (event) {
-    if (event.keyCode === 13) {
-        var result = customers.find(({id}) => id === $("#searchCustomerId").val());
-        console.log(result);
-
-        $("#searchCustomerId").val(result.id);
-        $("#nameUpdate").val(result.name);
-        $("#addressUpdate").val(result.address);
-        $("#salaryUpdate").val(result.salary);
-
-    }
-});*/
-
-/**
- * clear input fields Values Method
- * */
-function clearUTextFields() {
-    searchCustomerId.value = '';
-    nameUpdate.value = '';
-    addressUpdate.value = '';
-    salaryUpdate.value = '';
-}
-
-/**
- * clear input fields Values Button
- * */
-$("#btnUclearC").click(function () {
-    clearUTextFields();
-});
-
-
-/**
- * Delete Model
- * */
-
-/*
-
-/!**
- * Delete Model
- * Search id Enter Pressed And Load TextFields
- * *!/
-$("#searchCIdDelete").keyup(function (event) {
-    if (event.keyCode === 13) {
-        var result = customers.find(({id}) => id === $("#searchCIdDelete").val());
-        console.log(result);
-
-        $("#searchCIdDelete").val(result.id);
-        $("#disabledNameDelete").val(result.name);
-        $("#disabledAddressDelete").val(result.address);
-        $("#disabledSalaryDelete").val(result.salary);
-
-    }
-});
-*/
-
-/**
- * clear input fields Values Method
- * */
-function clearDTextFields() {
-    searchCIdDelete.value = '';
-    disabledNameDelete.value = '';
-    disabledAddressDelete.value = '';
-    disabledSalaryDelete.value = '';
-}
-
-/**
- * clear input fields Values Button
- * */
-$("#btnDclearC").click(function () {
-    clearDTextFields();
-});
