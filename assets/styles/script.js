20aff25bfe4d90b3736a954c83b48eb671ec3f7d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-10-09 12:09:27 +0530

    Refactored Full Content

--- assets/styles/script.js
+++ assets/styles/script.js
@@ -9,4 +9,16 @@
 
         // $("#loader").css('display','none');
         $("#loader").fadeOut(1000);
+        let style = document.createElement("style");
+        style.innerHTML = `body::-webkit-scrollbar {display: block;}`;
+        document.head.appendChild(style);
+
+        let style2 = document.createElement("style");
+        style2.innerHTML = `main {margin-top: 70px;}`;
+        document.head.appendChild(style2);
 });
+    $(document).ready(function () {
+        let style = document.createElement("style");
+        style.innerHTML = `body::-webkit-scrollbar {display: none;}`;
+        document.head.appendChild(style);
+    })
